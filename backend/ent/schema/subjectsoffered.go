2586c0e16472d8dec61640781f1af1966d8925a6
B6107192 <pattapong999@gmail.com>
2021-01-14 15:57:41 +0700

    สร้าง entity InstallmentEntity เพื่อเก็บข้อมูลในการดาวน์และผ่อนชำระ - close #3

---
+++ backend/ent/schema/subjectsoffered.go
@@ -0,0 +1,33 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
+
+// SubjectsOffered holds the schema definition for the SubjectsOffered entity.
+type SubjectsOffered struct {
+	ent.Schema
+}
+
+// Fields of the SubjectsOffered.
+func (SubjectsOffered) Fields() []ent.Field {
+	return []ent.Field{
+
+		field.String("AMOUNT").NotEmpty(),
+		field.String("STATUS").NotEmpty(),
+	}
+}
+
+// Edges of the SubjectsOffered.
+func (SubjectsOffered) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("Subject", Subject.Type).
+			Ref("SubjectsOffered").Unique(),
+		edge.From("EducationLevel", EducationLevel.Type).
+			Ref("SubjectsOffered").Unique(),
+		edge.From("Year", Year.Type).
+			Ref("SubjectsOffered").Unique(),
+	}
+}
